f578b43e0e0974f7e897cdb9b4d64d0d7420c24d
melihcan <melihcanclk@hotmail.com>
2023-03-25 01:03:09 +0300

    renderin green sphere

--- examples/ray_class/main.cc
+++ examples/ray_class/main.cc
@@ -9,11 +9,43 @@
 #define FOCAL_LENGTH 1.0
 #define VIEWPORT_HEIGHT 2.0
 
+bool hit_sphere(const point3 &center, float radius, const ray &r)
+{
+    // ray-sphere intersection
+    // a = B dot B
+    // b = 2B dot (A - C)
+    // c = (A - C) dot (A - C) - R^2
+    // discriminant = b^2 - 4ac
+    // if discriminant < 0, no intersection
+    // if discriminant = 0, one intersection
+    // if discriminant > 0, two intersections
+    // if t > 0, the intersection is in front of the camera
+    // if t < 0, the intersection is behind the camera
+    // if t = 0, the intersection is on the camera
+    vec3 origin_to_center = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(origin_to_center, r.direction());
+    float c = dot(origin_to_center, origin_to_center) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray &r)
 {
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+    // if we hit the sphere, return the color of the sphere
+    point3 sphere_center(0, 0, -2);
+    float sphere_radius = 0.5;
+    if (hit_sphere(sphere_center, sphere_radius, r))
+    {
+        return color(0, 1, 0);
+    }
+    else
+    {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5 * (unit_direction.y() + 1.0);
+        // return background color
+        return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+    }
 }
 
 void render(const int image_height, const point3 origin, const point3 lower_left_corner, const point3 horizontal, const point3 vertical)
